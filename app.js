ef45e78449762fdce8d1ff78b9ca1280c2122be0
krish-199 <68241105+krish-199@users.noreply.github.com>
2021-06-15 11:41:54 +0530

    Updated app.js, updated dependencies, chenged docker compose, added new middleware, tried changing scss file

--- app.js
+++ app.js
@@ -4,21 +4,21 @@
 const express = require('express');
 const compression = require('compression');
 const session = require('express-session');
-const bodyParser = require('body-parser');
 const logger = require('morgan');
 const chalk = require('chalk');
 const errorHandler = require('errorhandler');
 const lusca = require('lusca');
 const dotenv = require('dotenv');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const flash = require('express-flash');
 const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const sass = require('node-sass-middleware');
+const sass = require('./middleware-saas');
 const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
+
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -65,20 +65,22 @@ app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(compression());
-app.use(sass({
+/*app.use(sass({
   src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
-}));
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  }));*/
+//.app.use(sass);  
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
   resave: true,
   saveUninitialized: true,
   secret: process.env.SESSION_SECRET,
   cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
-  store: new MongoStore({
-    url: process.env.MONGODB_URI,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
     autoReconnect: true,
   })
 }));
