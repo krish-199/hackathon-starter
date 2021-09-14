f6942871fef22a0d64e5767912ae87c4abb80103
krish-199 <68241105+krish-199@users.noreply.github.com>
2021-09-15 01:30:43 +0530

    Added to render to dashboard Added post fileupload render

--- controllers/home.js
+++ controllers/home.js
@@ -7,3 +7,14 @@ exports.index = (req, res) => {
     title: 'Home'
   });
 };
+
+exports.adminLte = (req, res) => {
+  res.render('admin-lte/Dashboard', {
+    title: 'Dashboard - Admin'
+  });
+};
+
+exports.postFileUpload = (req, res) => {
+  req.flash('success', { msg: 'File was uploaded successfully.' });
+  res.redirect('admin-lte/Dashboard');
+};
